58e0ca35b34091c95e8f509a6d0a484d85b5ed7b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-03-05 20:18:04 -0300

    feat:create header and footer and create default routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      Galeria
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
