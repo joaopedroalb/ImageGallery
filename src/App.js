5e0ba71e7fa31d20e222ce0842dce805ac5822ea
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-03-05 21:20:02 -0300

    chore: update global css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css'
+import {Link} from 'react-router-dom' 
 
 export default function Header() {
-  return (
-    <div className={styles.container}>Header</div>
-  )
+    return (
+        <header className={styles.container}>
+            <nav className={styles.nav}>
+                <Link to="/" className={styles.logo} arial-label="Pets - Home">
+                    Home
+                </Link>
+                <Link to="/login" className={styles.login}>
+                    Login / Criar
+                </Link>
+            </nav>
+        </header>
+    )
 }
