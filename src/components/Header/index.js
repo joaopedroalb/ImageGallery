43a8d96e63402c1726aac64e738c7c104ec26787
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-03-07 09:32:24 -0300

    chore: add svgs and update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './styles.module.css'
 import {Link} from 'react-router-dom' 
+import {ReactComponent as Pets} from '../../Assets/dogs.svg'
 
 export default function Header() {
     return (
         <header className={styles.container}>
             <nav className={styles.nav}>
                 <Link to="/" className={styles.logo} arial-label="Pets - Home">
-                    Home
+                    <Pets/>
                 </Link>
                 <Link to="/login" className={styles.login}>
                     Login / Criar
