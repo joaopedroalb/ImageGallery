69d19fae1185efb59c77e81a2dfa8c1d70c971f6
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-04-25 13:45:11 -0300

    feat: add getUser func in api and login form

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,7 +1,6 @@
 export const API_URL = 'https://dogsapi.origamid.dev/json'
 
 export function TOKEN_POST(body){
-    console.log(body)
     return {
         url:API_URL+'/jwt-auth/v1/token',
         options:{
@@ -10,6 +9,18 @@ export function TOKEN_POST(body){
                 'Content-Type': 'application/json',
             },
             body:JSON.stringify(body)
+        }
+    }
+}
+
+export function USER_GET(token){
+    return {
+        url:API_URL+'/api/user',
+        options:{
+            method:'GET',
+            headers: {
+                Authorization:'Bearer '+token,
+            },
         }
     }
 }
--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../../components/Forms/Button';
 import Input from '../../components/Forms/Input';
-import { TOKEN_POST } from '../../Utils/api';
+import { TOKEN_POST, USER_GET } from '../../Utils/api';
 import useForm from '../../Hooks/useForm';
 
 export default function LoginForm() {
     const username = useForm();
     const password = useForm()
+
+    useEffect(()=>{
+        const token = window.localStorage.getItem('token')
+        if(token)
+            getUser(token)
+    },[])
+
+    const getUser = async(token) =>{
+        const {url,options} = USER_GET(token)
+        const res = await fetch(url,options)
+        const json = await res.json()
+        console.log(json)
+    }
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -17,11 +30,10 @@ export default function LoginForm() {
                                                 username:username.value,
                                                 password:password.value
                                             })
-    
             const res = await fetch(url, options)
             const result = await res.json()
             window.localStorage.setItem('token',result.token)
-            console.log(result)
+            getUser(result.token)
         }
     }
 
