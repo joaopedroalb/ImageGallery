017e8fdb46777a60c5349219a2a82f97e9700d0b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-04-25 15:44:42 -0300

    feat: update userContext with validate and navigate logic

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from 'react'
-import { TOKEN_POST, USER_GET } from '../Utils/api'
+import React, { createContext, useCallback, useEffect, useState } from 'react'
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from '../Utils/api'
+import { useNavigate } from 'react-router-dom'
 
 export const UserContext = createContext()
 
@@ -9,13 +10,64 @@ export function UserProvider({ children }) {
     const [login,setLogin] = useState(null)
     const [loading,setLoading] = useState(false)
     const [error,setError] = useState(false)
+    const navigate = useNavigate()
+
+    const userLogout = useCallback(async() =>{
+        setError(null)
+        setUser(null)
+        setLogin(false)
+        setLoading(false)
+        window.localStorage.removeItem('token')
+        navigate('/teste')
+    },[navigate])
+
+    useEffect(()=>{
+        async function autoLogin(){
+            const token = window.localStorage.getItem('token')
+            if(token){
+                try{
+                    setError(null)
+                    setLoading(true)
+
+                    const {url,options} = TOKEN_VALIDATE_POST(token)
+                    const res = await fetch(url,options)
+
+                    if(!res.ok) 
+                        throw new Error('Token Invalido')
+
+                    await getUser(token)
+
+                }catch(err){
+                    userLogout()
+                }finally{
+                    setLoading(false)
+                }
+            }
+        }
+
+        autoLogin()
+    },[userLogout])
 
     async function userLogin(username,password){
-        const {url,options} = TOKEN_POST({username,password})
-        const tokenRes = await fetch(url,options)
-        const {token} = await tokenRes.json()
-        window.localStorage.setItem('token',token)
-        getUser(token)
+        try{
+            setError(null)
+            setLoading(true)
+
+            const {url,options} = TOKEN_POST({username,password})
+            const tokenRes = await fetch(url,options)
+            console.log(tokenRes)
+            if(!tokenRes.ok) throw new Error(`Error: Login Invalido`)
+
+            const {token} = await tokenRes.json()
+            window.localStorage.setItem('token',token)
+            await getUser(token)
+            navigate('/account')
+        }catch(err){
+            setError(err.message)
+            setLogin(false)
+        }finally{
+            setLoading(false)
+        }
     }
 
     async function getUser(token){
@@ -27,7 +79,7 @@ export function UserProvider({ children }) {
     }
 
     return (
-        <UserContext.Provider value={{user,userLogin}}>
+        <UserContext.Provider value={{user,userLogin,userLogout,error,loading,login}}>
             {children}
         </UserContext.Provider>
     )
