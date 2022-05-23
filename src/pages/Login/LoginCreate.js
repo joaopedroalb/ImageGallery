e5470ebbb3a9d9f2a369f8f66a94d3a2bb8cc020
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-23 13:35:30 -0300

    feat: init create user page

--- src/pages/Login/LoginCreate.js
+++ src/pages/Login/LoginCreate.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Button from '../../components/Forms/Button'
+import Input from '../../components/Forms/Input'
+import { UserContext } from '../../context/UserContext'
+import useForm from '../../Hooks/useForm'
+import { USER_POST } from '../../Utils/api'
+
+import styles from './logincreate.module.css'
 
 export default function LoginCreate() {
+
+  const username = useForm()
+  const email = useForm('email')
+  const password = useForm()
+
+  const {userLogin} = useContext(UserContext)
+
+  async function handleSubmit(event){
+    event.preventDefault()
+    const {url,options} = USER_POST({
+                          username:username.value,
+                          email:email.value,
+                          password:password.value
+                        })
+    const res = await fetch(url,options)
+
+    if(res.ok) userLogin(username.value,password.value)
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className={`animeLeft`}>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input 
+          label="Usuário"
+          type="text"
+          name="username"
+          {...username}
+        />
+        <Input 
+          label="Email"
+          type="email"
+          name="email"
+          {...email}
+        />
+        <Input 
+          label="Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
